e004138e67dab60c6cd8e15cbfe7eb7d9c3e67ed
Axat Bhardwaj <axat@solulab.com>
2022-01-22 15:26:41 +0530

    staking draft 1

--- Staking/Staking.sol
+++ Staking/Staking.sol
@@ -1,21 +1,22 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.4;
 
-struct Stakepage {
-    uint256 amount;
-    uint256 timestamp;
-}
-
-mapping (address => Stakepage[])stakeBook;
-
-
 contract Staking {
     event Staking(address indexed _staker, uint256 _amount);
 
+    mapping(address => Stakepage[]) stakeBook;
+
+    struct Stakepage {
+        uint256 amount;
+        uint256 timestamp;
+    }
+
     function stake(uint256 _amount) public payable {
-        Stakepage[] stakepages = stakeBook[msg.sender];
+        Stakepage[] storage stakepages = stakeBook[msg.sender];
         if (stakepages.length == 0) {
-            stakepages.push(Stakepage({amount: _amount, timestamp: block.timestamp}));
+            stakepages.push(
+                Stakepage({amount: _amount, timestamp: block.timestamp})
+            );
         } else {
             stakepages[0].amount += _amount;
             stakepages[0].timestamp = block.timestamp;
@@ -25,7 +26,10 @@ contract Staking {
     }
 
     function unstake(uint256 _amount) public {
-        require(_amount <= balance);
-        Staking(msg.sender, -_amount);
+        Stakepage[] memory stakepages = stakeBook[msg.sender];
+        require(stakepages.length != 0, "No staking to unstake");
+        require(stakepages[0].amount < _amount, "Not enough staked to unstake");
+        stakepages[0].amount -= _amount;
+        stakeBook[msg.sender] = stakepages;
     }
 }
