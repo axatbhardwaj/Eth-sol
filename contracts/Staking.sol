e831d5f8242e49436f5f258c0b883d0d43d424bb
Axat Bhardwaj <axat@solulab.com>
2022-01-24 14:25:32 +0530

    folder struct

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -22,15 +22,37 @@ contract Staking {
             stakepages[0].amount += _amount;
             stakepages[0].timestamp = block.timestamp;
         }
-        stakeBook[msg.sender] = stakepages;
         emit Stake(msg.sender, _amount);
+    }
+
+    function intrestcalc() {
+        Stakepage[] storage stakepages = stakeBook[msg.sender];
+        if (stakepages.length == 0) {
+            return;
+        }
+        uint256 totalStaked = stakepages[i].amount;
+        uint256 totalTime = block.timestamp - stakepages[i].timestamp;
+        if (totalTime > 12 weeks && totalTime < 24 weeks) {
+            uint256 interest = totalStaked * totalTime;
+            msg.sender.transfer(interest);
+        }
+        uint256 intrest = (totalStaked / 15) * 100;
+        msg.sender.transfer(intrest);
     }
 
     function unstake(uint256 _amount) public {
         Stakepage[] memory stakepages = stakeBook[msg.sender];
+        uint256 available_unstakeAmount;
         require(stakepages.length != 0, "No staking to unstake");
-        require(stakepages[0].amount < _amount, "Not enough staked to unstake");
-        stakepages[0].amount -= _amount;
-        stakeBook[msg.sender] = stakepages;
+        for (uint256 i = 0; i < stakepages.length; i++) {
+            available_unstakeAmount =
+                stakepages[i].amount +
+                available_unstakeAmount;
+        }
+        require(
+            _amount < available_unstakeAmount,
+            "Not enough staked to unstake"
+        );
+        emit Unstake(msg.sender, _amount);
     }
 }
