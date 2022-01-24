0985ccb99429ae9b1dfdaed537e0d3af99942a0e
Axat Bhardwaj <axat@solulab.com>
2022-01-24 18:23:04 +0530

    updated hardhat config final attempt 1

--- scripts/deploy_staking.js
+++ scripts/deploy_staking.js
@@ -8,11 +8,10 @@ async function main() {
   const {ethers} = require('hardhat')
 	const Staking = await ethers.getContractFactory('Staking')
 	console.log('Deploying Staking...')
-	const bank = await Staking.deploy(0xdc4b03A13b747b8DCffECE8791a096bE570f0598)
+	const bank = await Staking.deploy("0xdc4b03A13b747b8DCffECE8791a096bE570f0598")
 	await bank.deployed()
 	console.log('Staking deployed to:', bank.address)
 }
-main()
 
 main()
   .then(() => process.exit(0))
