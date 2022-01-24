a04024142c8298dba4c624ea6c6e661cdf468958
Axat Bhardwaj <axat@solulab.com>
2022-01-24 17:00:00 +0530

    deployment script draft

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,19 +1,11 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Greeter", function () {
+describe("Staking", function () {
   it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
-
-    expect(await greeter.greet()).to.equal("Hello, world!");
-
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-
+    const Staking = await ethers.getContractFactory("Staking");
+    const staking = await Staking.deploy("");
+    await staking.deployed();
     // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
   });
 });
